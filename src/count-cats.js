2b8051c351efe819dd02b5819d72f3f7cd0149a2
Dmitry <riiba228@gmail.com>
2020-09-19 23:03:40 +0300

    fix: delete throw error in count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(arr) {
-  throw new CustomError('Not implemented');
   return arr.flat()
     .filter(item => item === '^^').length
 };
